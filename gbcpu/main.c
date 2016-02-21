2e42246f6e8af7e240995d10b97dcd3bbbe9ed05
Michael Steil <mist64@mac.com>
2016-02-22 00:07:12 +0100

    first three opcodes

--- gbcpu/main.c
+++ gbcpu/main.c
@@ -2,14 +2,58 @@
 //  main.c
 //  gbcpu
 //
-//  Created by Michael Steil on 21/02/16.
+//  Created by Lisa Brodner on 2016-02-16.
 //  Copyright © 2016 Lisa Brodner. All rights reserved.
 //
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	printf("Hello, World!\n");
-    return 0;
+int
+main(int argc, const char * argv[])
+{
+	uint8_t RAM[65536];
+	uint16_t pc = 0;
+	uint16_t sp = 0;
+	
+	uint8_t a = 0;
+	
+	// registers, can be used as 16 bit registers in the following combinations:
+	// a,f,
+	// b,c,
+	// d,e,
+	// h,l
+
+	
+	FILE *f = fopen("/Users/mist/Documents/git/gbcpu/gbcpu/DMG_ROM.bin", "r");
+	fread(RAM, 256, 1, f);
+	fclose(f);
+	
+	for (;;) {
+		uint8_t opcode = RAM[pc++];
+		printf("0x%x\n", opcode);
+		
+		switch (opcode) {
+			case 0: // nop
+				break;
+				
+			case 0x31: { // LD SP,d16 - 3
+				uint8_t d16l = RAM[pc++];
+				uint8_t d16u = RAM[pc++];
+				
+				uint16_t d16 = (d16u << 8) + d16l;
+//				printf("%x%x %x\n", d16l, d16u, d16);
+				sp = d16;
+				break;
+			}
+			case 0xaf: // XOR A - 1 // XOR A,A
+				a = 0;
+				break;
+				
+			default:
+				printf("Unknown Opcode\n");
+				return 1;
+		}
+	}
+	
+	return 0;
 }
