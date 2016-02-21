816f70783ad7875272a1a7326f5615a423a33d34
Michael Steil <mist64@mac.com>
2016-02-22 00:32:36 +0100

    ...

--- gbcpu/main.c
+++ gbcpu/main.c
@@ -8,20 +8,41 @@
 
 #include <stdio.h>
 
+uint8_t RAM[65536];
+uint16_t pc = 0;
+uint16_t sp = 0;
+
+uint8_t a = 0;
+uint8_t h = 0;
+uint8_t l = 0;
+
+// registers, can be used as 16 bit registers in the following combinations:
+// a,f,
+// b,c,
+// d,e,
+// h,l
+
+union reg16_t {
+	uint16_t full;
+	struct {
+		uint8_t low;
+		uint8_t high;
+	};
+};
+
+union reg16_t hl;
+
+void
+ld16(uint16_t *r16)
+{
+	uint8_t d16l = RAM[pc++];
+	uint8_t d16h = RAM[pc++];
+	*r16 = (d16h << 8) | d16l;
+}
+
 int
 main(int argc, const char * argv[])
 {
-	uint8_t RAM[65536];
-	uint16_t pc = 0;
-	uint16_t sp = 0;
-	
-	uint8_t a = 0;
-	
-	// registers, can be used as 16 bit registers in the following combinations:
-	// a,f,
-	// b,c,
-	// d,e,
-	// h,l
 
 	
 	FILE *f = fopen("/Users/mist/Documents/git/gbcpu/gbcpu/DMG_ROM.bin", "r");
@@ -36,15 +57,12 @@ main(int argc, const char * argv[])
 			case 0: // nop
 				break;
 				
-			case 0x31: { // LD SP,d16 - 3
-				uint8_t d16l = RAM[pc++];
-				uint8_t d16u = RAM[pc++];
-				
-				uint16_t d16 = (d16u << 8) + d16l;
-//				printf("%x%x %x\n", d16l, d16u, d16);
-				sp = d16;
+			case 0x21: // LD HL,d16 - 3
+				ld16(&hl.full);
 				break;
-			}
+			case 0x31: // LD SP,d16 - 3
+				ld16(&sp);
+				break;
 			case 0xaf: // XOR A - 1 // XOR A,A
 				a = 0;
 				break;
