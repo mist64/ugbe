abaf1b4d91a7581f199c663bcad8814927b3aa75
Michael Steil <mist64@mac.com>
2016-02-26 12:07:55 +0100

    moved ppu into its own source file

---
+++ gbppu/ppu.h
@@ -0,0 +1,14 @@
+//
+//  ppu.h
+//  gbppu
+//
+//  Created by Michael Steil on 2016-02-26.
+//  Copyright © 2016 Michael Steil. All rights reserved.
+//
+
+#ifndef ppu_h
+#define ppu_h
+
+#include <stdio.h>
+
+#endif /* ppu_h */
